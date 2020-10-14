7fcb80d3bec448c1da10ffd039e1f9c92541a725
Sami Laine <sami.v.laine@gmail.com>
2020-10-14 14:50:02 +0200

    Remove secret key

--- banner/banner/settings.py
+++ banner/banner/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .local_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 're2h6u*%lhricuc$46_o!^#jc_^$i_p(pk_e0^$4l*f9un0-c)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
