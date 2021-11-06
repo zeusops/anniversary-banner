2f6960ea91d51a6156b8de55b6730824303e0f9e
Sami Laine <sami.v.laine@gmail.com>
2021-11-06 22:14:48 +0100

    Remove sides, fit more teams at once

--- banner/image/views.py
+++ banner/image/views.py
@@ -7,7 +7,8 @@ from PIL import Image, ImageDraw, ImageFont
 
 from .models import BannerConfigEntry, Side, Team
 
-TEAMS_LIMIT = 9
+TEAMS_PER_COLUMN = 5
+TEAMS_LIMIT = TEAMS_PER_COLUMN * 3
 WHITE = (229, 229, 229)
 
 def _get_config(name):
@@ -54,22 +55,40 @@ def _draw_points(target, side_left, side_right, debug=False):
 
 def _draw_leaderboard(target, debug=False):
     draw = ImageDraw.Draw(target)
-    draw.line((650, 50, 650, 350), fill=WHITE, width=2)
+    #draw.line((650, 50, 650, 350), fill=WHITE, width=2)
+    wx, wy, wrx, wry, _ = _get_config('wins')
+    nx, ny, nrx, nry, _ = _get_config('names')
 
     font_leaderboard = ImageFont.truetype('DejaVuSans-Bold.ttf', 40)
     font_headings = ImageFont.truetype('DejaVuSans-Bold.ttf', 18)
-    draw.text((700, 50), "Top teams", font=font_leaderboard, fill=WHITE)
-    draw.text((700, 110), "wins", fill='gray', font=font_headings, anchor="ls")
-    draw.text((770, 110), "name", fill='gray', font=font_headings)
+    #draw.text((700, 50), "Top teams", font=font_leaderboard, fill=WHITE)
 
     font_teams = ImageFont.truetype('DejaVuSans-Bold.ttf', 20)
     teams = Team.objects.filter(side__active=True)[:TEAMS_LIMIT]
-    for i, team in enumerate(teams):
-        wins = str(team.points)
-        name = team.name
-        colour = team.side.colour
-        draw.text((715, 150 + i * 25), wins, fill=WHITE, font=font_teams)
-        draw.text((770, 150 + i * 25), name, fill=colour, font=font_teams)
+    longest_name = ""
+    for team in teams:
+        if len(team.name) > len(longest_name):
+            longest_name = team.name
+    name_size = draw.textsize(longest_name, font_teams)
+    padding = name_size[0] + 70 + 20
+    #print(f"{padding=}")
+    #print(f"{longest_name=}")
+    team_chunks = _chunk_list(teams, TEAMS_PER_COLUMN)
+    for i, chunk in enumerate(team_chunks):
+        draw.text((wx + i * padding, wy), "points", fill='gray', font=font_headings, anchor="ls")
+        draw.text((nx + i * padding, ny), "name", fill='gray', font=font_headings)
+        for j, team in enumerate(chunk):
+            wins = str(team.points)
+            name = team.name
+            colour = team.side.colour
+            draw.text((wrx + i * padding, wry + j * 25), wins, fill=WHITE, font=font_teams)
+            draw.text((nrx + i * padding, nry + j * 25), name, fill=colour, font=font_teams)
+
+def _chunk_list(iterable, chunk_size):
+    d = {}
+    for i, x in enumerate(iterable):
+        d.setdefault(i//chunk_size, []).append(x)
+    return(d.values())
 
 def generate_banner(debug=False, rainbow=False):
     sides = Side.objects.filter(active=True)
@@ -77,9 +96,9 @@ def generate_banner(debug=False, rainbow=False):
     side_right = sides[1]
 
     logos = [
-        ('logo_main', 'media/image/logo_text_resize.png'),
-        ('logo_left', side_left.logo),
-        ('logo_right', side_right.logo),
+        ('logo_main', 'media/image/logo_text_wide_resize.png'),
+        #('logo_left', side_left.logo),
+        #('logo_right', side_right.logo),
     ]
 
     bgcolour = 'black'
@@ -87,12 +106,12 @@ def generate_banner(debug=False, rainbow=False):
         bgcolour = (randint(0, 255), randint(0, 255), randint(0, 255))
     banner = Image.new('RGB', (1000, 400), color = bgcolour)
     _draw_images(logos, banner, debug)
-    _draw_points(banner, side_left, side_right, debug)
+    #_draw_points(banner, side_left, side_right, debug)
     _draw_leaderboard(banner, debug)
 
     return banner
 
-@cache_page(60)
+@cache_page(15)
 def banner(request):
     debug = request.GET.get('debug', 'false') == "true"
     rainbow = 'rainbow' in request.GET
